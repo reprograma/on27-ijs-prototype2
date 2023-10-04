ccfcd77e8acd46821a709b086f5e49f4968371d4
Camila Lima <camilam.oliveiralima@gmail.com>
2023-10-04 09:41:26 -0300

    Resoluçao exercicio para casa

--- exercicios/para-casa/resolucao/classes/Bank.js
+++ exercicios/para-casa/resolucao/classes/Bank.js
@@ -3,19 +3,15 @@ export class Bank {
 	bankName;
     #transferTax = 0;
 
+	static createdBanks = [];
 
-  static createdBanks = [];
-
-	// constructor(name, age) {
-	// 	if (age < 18) {
-	// 		return new Error(
-	// 			'É necessário ter mais de 18 anos para ser um motorista'
-	// 		);
-	// 	}
-	// 	this.name = name;
-	// 	this.age = age;
-    // this.constructor.drivers.push({ name: name, age: age });
-	// }
+	constructor(bankCode, bankName) {
+		this.bankCode = bankCode;
+		this.bankName = bankName;
+		this.constructor.createdBanks.push(
+			{ bankCode: bankCode, qtdClients: 0 }
+		);
+	}
 
 	get transferTax() {
 		return this.#transferTax;
@@ -24,23 +20,4 @@ export class Bank {
 	set transferTax(newTransferTax) {
 		this.#transferTax = newTransferTax;
 	}
-
-	// runDrive(amount) {
-	// 	this.#amountEarned += amount;
-	// 	this.numberOfRides++;
-	// }
-
-    // static numberOfDrivers() {
-	// 	console.log(`O total de motoristas cadastradas é: ${this.drivers.length}`);
-	// }
-
-	// static ageAverage() {
-	// 	const totalOfDrivers = this.drivers.length;
-
-    // if(totalOfDrivers === 0) return;
-
-	// 	const ageSum = this.drivers.reduce((total, motorista) => total + motorista.age, 0);
-	// 	const ageAverage = (ageSum / totalOfDrivers).toFixed(2);
-	// 	console.log(`A média de idade das motoristas é de: ${ageAverage}`);
-	// }
 }
--- exercicios/para-casa/resolucao/classes/Client.js
+++ exercicios/para-casa/resolucao/classes/Client.js
@@ -1,70 +1,24 @@
-import { Bank } from "./Driver.js";
+import { Person } from "./Person.js";
 
-export class Client {
-	name;
-	cpf;
-    banks = []
-
-  static passengers = [];
-
-	constructor(name, age, password) {
-		this.name = name;
-		this.age = age;
-		this.#password = password;
-    this.constructor.passengers.push({ name: name, age: age });
-	}
-
-	get amountSpent() {
-		return this.#amountSpent;
-	}
-
-	set amountSpent(newAmountSpent) {
-		this.#amountSpent = newAmountSpent;
-	}
+export class Client extends Person {
+	banks = [];
 
 	addBank(bank) {
-		if(bank instanceof Bank) {
-			this.bank != banks;
-			console.log('Cliente adicionado com sucesso.');
-		} else {
-			console.log('Esse cliente já possui conta.');
+		if (this.banks.includes(bank)) {
+			return console.log("Cliente já tem esse banco associado a ele.");
 		}
+
+		this.banks.push(bank);
+		bank.qtdClients++;
 	}
 
 	removeBank(bank) {
-		if(bank instanceof Bank) {
-			this.bank == banks;
-			console.log('Cliente removido com sucesso.');
-		} else {
-			console.log('Esse cliente não possui conta.');
+		if (!this.banks.includes(bank)) {
+			return console.log("Cliente não tem esse banco associado a ele.");
 		}
+
+		this.banks.splice(this.banks.indexOf(bank), 1);
+		bank.qtdClients--;
 	}
-    
 
-// 	requestDrive(driver, amount, password) {
-// 		if (!(driver instanceof Driver)) {
-// 			console.log('Motorista inválido!');
-// 			return;
-// 		}
-// 		if (password !== this.#password) {
-// 			console.log(`${this.name}, sua senha está incorreta!`);
-// 			return;
-// 		}
-// 		this.#amountSpent -= amount;
-// 		driver.runDrive(amount);
-// 	}
-
-//   static numberOfPassengers() {
-// 		console.log(`O total de passageiras cadastradas é: ${this.passengers.length}`);
-// 	}
-
-// 	static ageAverage() {
-// 		const totalOfPassengers = this.passengers.length;
-
-//     if(totalOfPassengers === 0) return;
-
-// 		const ageSum = this.passengers.reduce((total, motorista) => total + motorista.age, 0);
-// 		const ageAverage = (ageSum / totalOfPassengers).toFixed(2);
-// 		console.log(`A média de idade das passageiras é de: ${ageAverage}`);
-// 	}
 }
