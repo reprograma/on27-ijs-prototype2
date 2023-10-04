ec8b912403be96b506b620d165cabb9e3a921d46
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-10-04 19:59:50 -0300

    Add desafio semanal - Prototype 2

---
+++ exercicios/joseane-guedes-para-casa/resolucao/classes/Bank.js
@@ -0,0 +1,41 @@
+export class Bank {
+    bankCode;
+    bankName;
+    #transferTax;
+    totalClientes
+
+    static createdBanks = [];
+
+    constructor(bankCode, bankName, transferTax) {
+        this.bankCode = bankCode;
+        this.bankName = bankName;
+        this.#transferTax = transferTax;
+        this.totalClientes = 0;
+        Bank.createdBanks.push({ bankCode: this.bankCode, totalClientes: this.totalClientes });
+    }
+
+    get transferTax() {
+        return this.#transferTax;
+    }
+
+    set transferTax(newTransferTax) {
+        this.#transferTax = newTransferTax;
+    }
+}
+
+
+//testando 1,2,3 
+
+const bank1 = new Bank(666, 'BeastBank', 0.01);
+const bank2 = new Bank(999, 'KarmaBank', 0.02);
+
+console.log(Bank.createdBanks)
+console.log("Banco 1:", bank1);
+console.log("Banco 2:", bank2);
+
+console.log(bank1.transferTax)
+console.log(bank2.transferTax)
+
+
+
+
