366966a59e9e1b5aacbf15e0723c19a01297cbc2
Beatriz Locatelli <beatrizglocatelli@gmail.com>
2023-10-04 23:20:23 -0300

    Entrega do exercicio da semana 3 - Prototype

---
+++ exercicios/para-casa/resolucao/beatriz-locatelli/classes/Client.js
@@ -0,0 +1,45 @@
+import { Bank } from "./Bank.js";
+
+export class Client {
+    name;
+    #cpf;
+    banks = [];
+
+    constructor(name, cpf) {
+        this.name = name;
+        this.#cpf = cpf;
+    }
+
+    addBank(bank) {
+        if (!(bank instanceof Bank)) {
+            console.log('Banco inválido!');
+            return;
+        }
+        if (this.banks.includes(bank)) {
+            throw (`Banco já cadastrado ao cliente ${this.name}`)
+        }
+        this.banks.push(bank)
+        console.log(`Banco ${bank.bankCode} adicionado ao(à) cliente ${this.name}`);
+        
+    }
+
+    removeBank(bank) {
+        if (!(bank instanceof Bank)) {
+            console.log('Banco inválido!');
+            return;
+        }
+        let index = this.banks.indexOf(bank);
+      
+        if(index === -1) {
+          throw ('Banco inválido');
+        } else {
+          this.banks = this.banks.filter(bankCode => bankCode !== bank);
+          console.log(`O Banco ${bank.bankCode} foi removido do(a) cliente ${this.name}.`);
+        }
+      }
+    
+
+    get cpf() {
+        return this.#cpf;
+    }
+}
--- exercicios/para-casa/resolucao/index.js
+++ exercicios/para-casa/resolucao/index.js
@@ -1,63 +1,71 @@
 //exercicios feitos pela profa aqui
-import { Bank, bank1, bank2 } from './classes/Bank.js';
-import { BankAccount } from './classes/BankAccount.js';
-import { client1, client2 } from './classes/Client.js';
+import { Bank } from './beatriz-locatelli/classes/Bank.js';
+import { Client } from './beatriz-locatelli/classes/Client.js'
+import { BankAccount } from './beatriz-locatelli/classes/BanksAccount.js';
 
+console.log(Bank.createdBanks); // [ ]
+
+const bank1 = new Bank(100, 'LuaBank', 0.01); // Instanciação de um objeto Bank.
+console.log(bank1); // { bankCode: 100, bankName: 'LuaBank' }
+// Agora a propriedade estática createdBanks é uma
+// array que contém 1 objeto, que corresponde ao banco criado.
+// O objeto possui o código do banco e a quantidade de clientes (que inicialmente é 0):
+console.log(Bank.createdBanks); // [ { bankCode: 100, qtdClients: 0 } ]
+
+console.log(bank1.transferTax); // 0.01
+bank1.transferTax = 0.02
+console.log(bank1.transferTax); // 0.02
+
+const bank2 = new Bank(200, 'BiaBank', 0.05); 
+console.log(bank2); 
+console.log(Bank.createdBanks); 
+
+
+const client1 = new Client('Maria', 123); // Instanciação de um objeto Client.
+console.log(client1); // { name: 'Maria', banks: [] }
+console.log(client1.cpf); // 12345678900
+
+// Adicionando um banco a um cliente
 client1.addBank(bank1); // Banco 100 adicionado à cliente Maria
-client1.addBank(bank2); // Banco 200 adicionado à cliente Maria
-client1.removeBank(bank2); // Banco 200 removido da cliente Maria
-// console.log(client1); // Client { name: 'Maria', banks: [ Bank { bankCode: 100, bankName: 'LuaBank' } ] }
+console.log(client1);// { name: 'Maria', banks: [ Bank { bankCode: 100, bankName: 'LuaBank' } ] }
 
-client2.addBank(bank2); // Banco 200 adicionado à cliente Sandra
-// console.log(client2); // Client { name: 'Sandra', banks: [ Bank { bankCode: 200, bankName: 'AnotherBank' } ] }
-console.log(Bank.createdBanks); // [ { bankCode: 100, qtdClients: 1 }, { bankCode: 200, qtdClients: 1 } ]
+// Removendo um banco de um cliente
+client1.removeBank(bank1); // Banco 100 removido da cliente Maria
+console.log(client1); // { name: 'Maria', banks: [] }
 
-const bankAccount1 = new BankAccount(client1, bank1, 1111, 2222);
-// console.log(bankAccount1);
-/*
-	CONTA CRIADA:
-	BankAccount {
-  client: Client { name: 'Maria', banks: [ [Bank] ] },
-  bank: Bank { bankCode: 100, bankName: 'LuaBank' },
-  accountNumber: 1111,
-  agencyNumber: 2222,
-  qtdWithdrawal: 0
-}
-*/
+const client2 = new Client('Bia', 321); 
+console.log(client2); 
+console.log(client2.cpf); 
 
-const bankAccount2 = new BankAccount(client1, bank2, 3333, 4444);
-// console.log(bankAccount2);
-/*
-	ERRO:
-	Cliente do CPF 123456789 não possui conta no banco AnotherBank
-}
-*/
+// Adicionando um banco a um cliente
+client2.addBank(bank2); 
+console.log(client2);
 
-const bankAccount3 = new BankAccount(client2, bank2, 5555, 6666);
-// console.log(bankAccount3);
-/*
-	CONTA CRIADA:
-	BankAccount {
-  client: Client { name: 'Sandra', banks: [ [Bank] ] },
-  bank: Bank { bankCode: 200, bankName: 'AnotherBank' },
-  accountNumber: 5555,
-  agencyNumber: 6666,
-  qtdWithdrawal: 0
-}
-*/
+// Removendo um banco de um cliente
+client2.removeBank(bank2);
+console.log(client2); 
 
-bankAccount1.creditAmount(1310); // O novo saldo da conta é: R$ 1310
-bankAccount1.debitAmount(300); // O novo saldo da conta é: R$ 1010
 
-bankAccount1.transferTo(bankAccount3, 1500);
-// Essa transferência terá uma taxa de 1%, pois se trata de uma transferência entre bancos diferentes.
-// Saldo insuficiente para realizar a transferência. Seu saldo atual é de 1010. Para realizar essa transferência você precisa ter 1515 em conta.
-
-bankAccount1.transferTo(bankAccount3, 1000);
-// Essa transferência terá uma taxa de 1%, pois se trata de uma transferência entre bancos diferentes.
-// O saldo atual da conta de origem é de R$ 0
-// O saldo atual da conta de destino é de R$ 1000
-
-bankAccount3.closeAccount(); // Você possui um saldo de R$ 697. Para encerrar a conta é necessário que o saldo seja igual a zero.
-bankAccount1.closeAccount(); // Conta encerrada!
+const bankAccount1 = new BankAccount(client1, bank1, 1111, 2222); // Instanciação de um objeto BankAccount.
 console.log(bankAccount1);
+// { 
+//   client: Client { name: 'Maria', banks: [ [Bank] ] },
+//   bank: Bank { bankCode: 100, bankName: 'LuaBank' },
+//   accountNumber: 1111,
+//   agencyNumber: 2222,
+// }
+
+console.log(bankAccount1.balance); // 0
+
+const bankAccount2 = new BankAccount(client2, bank2, 5555, 6666); // Instanciação de um objeto BankAccount.
+console.log(bankAccount2);
+
+// Creditando dinheiro na conta
+bankAccount1.credit(1000); // O novo saldo da conta é: R$ 1000
+
+// Debitando dinheiro da conta
+bankAccount1.debit(300); // O novo saldo da conta é: R$ 700
+
+// Transferindo de uma conta para outra
+bankAccount1.transferTo(bankAccount2, 200);
+// O saldo atual da conta de origem é de R$ 500
