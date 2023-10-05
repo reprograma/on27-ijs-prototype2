9ccbf279591429d04f8bb28e9bc26bc17f7467bf
Ana Carolina Zago <carolina.anazago@gmail.com>
2023-10-04 21:21:20 -0300

    Exercício 1

--- exercicios/para-casa/resolucao/classes/Bank.js
+++ exercicios/para-casa/resolucao/classes/Bank.js
@@ -1,12 +1,34 @@
-class Bank {
+export class Bank {
     bankCode;
     bankName;
-    transferTax;
-    createdBanks = []
+    #transferTax;
+    numbersClients;
+
+    static createdBanks = []
 
     constructor(bankCode, bankName, transferTax) {
         this.bankCode = bankCode;
         this.bankName = bankName;
-        this.transferTax = transferTax;
+        this.#transferTax = transferTax;
+        this.numbersClients = 0;
+
+        // salvar os objs dentro da array createdBanks. Ele entra no construtor, faz as associações e popula createdBanks
+        this.constructor.createdBanks.push({ bankCode, numbersClients: this.numbersClients });
+    }
+
+    get transferTax() {
+        return this.#transferTax;
+    }
+
+    set transferTax(newTransferTax) {
+        this.#transferTax = newTransferTax;
     }
 }
+
+// criando um novo banco
+const bank1 = new Bank(100, 'LuaBank', 0.01);
+console.log(bank1);
+console.log(Bank.createdBanks);
+console.log(bank1.transferTax); 
+bank1.transferTax = 0.02
+console.log(bank1.transferTax);
